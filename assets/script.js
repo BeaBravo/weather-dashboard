6bad8a6b33d4db836237f1af9a9893ae7c02dba6
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-04 19:39:28 -0700

    fetches data in the weather api

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,39 @@
 //DEPENDENCIES
-var searchButton = $("#search-button");
+var formEl = $("#city-form");
+var cityNameEl = $('input[name="city-name"]');
+
 //DATA
+var APIkey = "271703820aa14bd16fc1a78b4794a1ec";
+var requestedURL;
 
 //FUNCTIONS
-
 function searchCity(event) {
   event.preventDefault();
+  var city = cityNameEl.val().trim();
 
-  console.log("you clicked the button!");
+  //lookup city in API
+
+  fetchCityDate(city);
+  return city;
+}
+
+function fetchCityDate(city) {
+  requestedURL =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    "&limit=5&appid=" +
+    APIkey;
+
+  fetch(requestedURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
 
 //USER INTERACTIONS
-searchButton.on("submit", searchCity);
+formEl.on("submit", searchCity);
 
 //INITIALIZATION
