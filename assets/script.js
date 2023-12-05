b63c79e5bc76603de49ab60a2b7a6c95ddcaa7ac
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-05 11:17:41 -0700

    limit search results to 1 response

--- assets/script.js
+++ assets/script.js
@@ -9,28 +9,54 @@ var requestedURL;
 //FUNCTIONS
 function searchCity(event) {
   event.preventDefault();
-  var city = cityNameEl.val().trim();
+  var city = cityNameEl.val();
 
   //lookup city in API
 
-  fetchCityDate(city);
+  fetchCityData(city);
   return city;
 }
 
-function fetchCityDate(city) {
+function fetchCityData(city) {
+  var latitude;
+  var longitude;
+
   requestedURL =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
-    "&limit=5&appid=" +
+    "&limit=1&appid=" +
     APIkey;
+
+  console.log(city);
 
   fetch(requestedURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      latitude = data[0].lat;
+      longitude = data[0].lon;
+
+      console.log("latitude", latitude);
+
+      console.log("long", longitude);
+
+      console.log("data", data);
+
+      return;
     });
+
+  //   fiveDayURL =
+  //     "api.openweathermap.org/data/2.5/forecast?lat=" +
+  //     latitude +
+  //     "&lon=" +
+  //     longitude +
+  //     "&appid=" +
+  //     APIkey;
+
+  //   fetch(fiveDayURL).then(function (response) {
+  //     console.log(response);
+  //   });
 }
 
 //USER INTERACTIONS
